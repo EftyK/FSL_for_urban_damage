efa2363e310d0f3150faa1e3aae39c3c67997755
Eftychia <efty.koukouraki@gmail.com>
2021-01-25 13:27:02 +0000

    Add legend. Remove x axis text. Add different colors for models.

--- utils/plot_bar_charts.py
+++ utils/plot_bar_charts.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.font_manager
-
+import matplotlib.patches as patches
 
 ##### DEFINITIONS ######
+color_palette = ['#32a852', '#3289a8', '#8b57cf']
+
 def plot_row(row, data):
-    rects = row.bar(models, data) #, color='#3289a8')
-    row.set_xticklabels(models, rotation=70, fontname='Rekha')
+    rects = row.bar(models, data, color=color_palette) # set different colors for each model
+    # row.set_xticklabels(models, rotation=70, fontname='Rekha')
+    row.get_xaxis().set_visible(False)
     for rect in rects:
         height = rect.get_height()
         row.text(rect.get_x() + rect.get_width() / 2, height,'{}'.format(height), ha='center', va='bottom', fontname='Rekha', weight='bold')
@@ -62,8 +65,8 @@ fig, axes = plt.subplots(len(classes), len(metrics), sharex=True)
 
 for row, i in zip(axes, range(len(classes))):
     for col  in row:
-        plot_row(row[0], precision[i])
-        plot_row(row[1], recall[i])
+        plot_row(row[0], precision[i]) 
+        plot_row(row[1], recall[i]) 
         plot_row(row[2], f_score[i])
 
 ##### ADD TITLES FOR COLUMNS AND ROWS ######
@@ -73,6 +76,15 @@ for ax, col in zip(axes[0], metrics):
 for ax, row in zip(axes[:,0], classes):
     ax.set_ylabel(row, rotation=45, fontname='Rekha')
     ax.yaxis.set_label_coords(-0.1,0.1)
+
+
+##### DEFINE LEGEND ######
+
+rect1 = patches.Rectangle((0,0),1,1, color='#32a852', label='model1')
+rect2 = patches.Rectangle((0,0),1,1, color='#3289a8', label='model2')
+rect3 = patches.Rectangle((0,0),1,1, color='#8b57cf', label='model3')
+
+fig.legend(handles=[rect1, rect2, rect3], loc="upper left")
 
 plt.tight_layout()
 plt.show()
