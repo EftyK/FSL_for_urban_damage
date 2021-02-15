0bea6e16f41a0f0aee0e1123840ff1e85b9dbbfb
Eftychia <efty.koukouraki@gmail.com>
2021-02-15 11:41:00 +0000

    Add script tool for image subtraction.

---
+++ utils/image_subtraction.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from PIL import ImageChops
+import argparse
+ 
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img1', required=True, metavar='/path/to/img1', help="Path to first image")
+    parser.add_argument('--img2', required=True, metavar='/path/to/img2', help="Path to second image")
+    parser.add_argument('--diff', required=True, metavar='/path/to/img_diff', help="Path to save image difference")
+    return parser.parse_args()
+
+args = parser()
+
+im1 = Image.open(args.img1) 
+im2 = Image.open(args.img2)
+
+diff = ImageChops.subtract(im2, im1)
+
+diff.save(args.diff)
